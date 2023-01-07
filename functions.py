5d22f6f3f369049235349d8934508003679b959d
seckinsosyal <seckinsosyal@yahoo.com>
2023-01-07 15:00:23 +0300

    now working

--- functions.py
+++ functions.py
@@ -1,12 +1,21 @@
 FILEPATH = "todos.txt"
 
-def get_todos():
-    with open(FILEPATH, "r") as file:
-        todos_local = file.readlines()
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos, filepath = FILEPATH):
-    with open(filepath, "w") as file:
-        file.writelines(todos)
-    return None
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
--- web.py
+++ web.py
@@ -2,7 +2,6 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
-
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -17,7 +16,7 @@ st.write("This app is to increase your productivity")
 st.checkbox("Buy grocery")
 st.checkbox("Throw the trash")
 
-for todo in enumerate(todos):
+for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
